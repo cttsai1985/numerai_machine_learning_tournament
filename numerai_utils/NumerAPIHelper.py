dfbd72e992ed29e6f71dc01bead5a02496afbdec
Chia-Ta Tsai <cttsai@Outlook.com>
2021-04-02 11:17:38 -0400

    new file downloading

--- numerai_utils/NumerAPIHelper.py
+++ numerai_utils/NumerAPIHelper.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from glob import glob
 from typing import Optional, List
 from pathlib import Path
 from numerapi import NumerAPI
@@ -6,46 +8,83 @@ from numerapi import NumerAPI
 
 class NumerAPIHelper:
     def __init__(
-            self, data_root_path: Optional[str] = None, model_root_path: Optional[str] = None,
-            api: Optional[NumerAPI] = None):
+            self, root_dir_path: Optional[str] = None, api: Optional[NumerAPI] = None):
         # TODO: wrap this into a json to init
         self.api: NumerAPI = NumerAPI() if api is None else api
-        self.data_root_path: str = "../input/numerai_datasets/" if not data_root_path else data_root_path
-        print(f"current data root: {self.data_root_path}")
-        self.model_root_path: str = "../input/numerai_models/" if not model_root_path else model_root_path
-        print(f"current model root: {self.model_root_path}")
-        self.valid_data_types = [
-            "live", "training", "validation", "test", "max_test_era", "tournament", "tournament_ids",
+
+        self.root_dir_path: str = os.path.join("..", "input", "numerai_tournament_resource")
+        if root_dir_path:
+            self.root_dir_path = root_dir_path
+
+        print(f"current root path: {self.root_dir_path}")
+
+        self.data_dir_path: str = os.path.join(self.root_dir_path, "latest_tournament_datasets")
+        print(f"data dir: {self.data_dir_path}")
+
+        self.valid_data_types: List[str] = [
+            "training", "validation", "live", "test", "max_test_era", "tournament", "tournament_ids",
             "example_predictions"]
 
-        self.dir_path_pattern: str = "numerai_{object_type}_{round:06d}"
+        self.current_round_str = f"{self.api.get_current_round():04d}"
+        self.round_identifier_template: str = f"numerai_tournament_round"
+        self.round_identifier_current: str = "_".join([self.round_identifier_template, self.current_round_str])
+        print(f"current round identifier: {self.round_identifier_current}")
+
+        self.round_identifier_latest: Optional[str] = None
+        self.round_identifier_latest = self.find_latest_round_identifier_
 
     @property
-    def dir_data_path_current_round_(self):
-        return os.path.join(self.data_root_path, self.dir_path_pattern.format(
-            object_type="datasets", round=self.api.get_current_round()))
+    def find_latest_round_identifier_(self, dir_path: Optional[str] = None) -> Optional[str]:
+        if self.round_identifier_latest:
+            return self.round_identifier_latest
+
+        if not dir_path:
+            dir_path = self.root_dir_path
+
+        candidates = sorted(
+            list(glob(os.path.join(dir_path, "_".join([self.round_identifier_template, "[0-9]*"])))))
+        if not candidates:  # initialized
+            return None
+
+        self.round_identifier_latest = Path(candidates[-1]).stem
+        return self.round_identifier_latest
 
     @property
     def dir_model_path_current_round_(self):
         return os.path.join(self.data_model_path, self.dir_path_pattern.format(
             object_type="models", round=self.api.get_current_round()))
 
+    def update_round_identifier_to_current(self, dir_path: Optional[str] = None):
+        if not dir_path:
+            dir_path = self.root_dir_path
+
+        file_path = os.path.join(dir_path, self.round_identifier_current)
+        print(f"update identifier at: {dir_path}")
+        with open(file_path, "w") as f:
+            pass
+        return self
+
     def download_latest_dataset(
             self, extension: str = "parquet", valid_data_types: Optional[List[str]] = None, refresh: bool = False):
-        dest_path = self.dir_data_path_current_round_
-        Path(dest_path).mkdir(parents=True, exist_ok=True)
+
+        if self.round_identifier_latest == self.round_identifier_current and not refresh:
+            print(f"skip download since datasets are up-to-date")
+            return
 
         if not valid_data_types:
             valid_data_types = self.valid_data_types
 
+        Path(self.data_dir_path).mkdir(parents=True, exist_ok=True)
+
         for data_type in valid_data_types:
             filename = f"numerai_{data_type}_data.{extension}"
-            if os.path.exists(os.path.join(dest_path, filename)) and not refresh:
-                print(f"skip download {filename} to {dest_path}\n")
-                continue
-
-            filename = self.api.download_latest_data(
-                data_type=data_type, extension="parquet", dest_path=dest_path, dest_filename=filename)
+            self.api.download_latest_data(
+                data_type=data_type, extension="parquet", dest_path=self.data_dir_path, dest_filename=filename)
             print(f"\n{filename} download finished.")
 
+        print(f"\nall requested datasets updated: {valid_data_types}")
+        self.update_round_identifier_to_current()
+
+        self.round_identifier_latest = self.round_identifier_current
+        print(f"current identifier at: {self.round_identifier_latest}")
         return self
