d0ffe7834460c4faa05bfe31baa84e093ef00735
Chia-Ta Tsai <cttsai@Outlook.com>
2021-08-01 20:34:13 -0400

    shared utils

--- ds_utils/Utils.py
+++ ds_utils/Utils.py
@@ -1,5 +1,8 @@
 import logging
+import os
+import yaml
 import re
+from pathlib import Path
 import pandas as pd
 import numpy as np
 from typing import Optional, Callable, Any, Dict, List, Tuple, Union
@@ -44,6 +47,33 @@ def natural_keys(text: str) -> List[str]:
 def natural_sort(list_of_str: List[str]) -> List[str]:
     list_of_str.sort(key=natural_keys)
     return list_of_str
+
+
+def load_yaml_configs(configs_file_path: str) -> Dict[str, Any]:
+    if not all([os.path.exists(configs_file_path), os.path.isfile(configs_file_path)]):
+        logging.info(f"configs does not exist: {configs_file_path}")
+        return dict()
+
+    with open(configs_file_path, 'r') as fp:
+        ret = yaml.load(fp, Loader=yaml.FullLoader)
+
+    return ret
+
+
+def save_yaml_configs(configs: Dict[str, Any], configs_file_path: str, debug: bool = False) -> Dict[str, Any]:
+    dir_path: str = Path(configs_file_path).parent
+    if not all([os.path.exists(dir_path), os.path.isdir(dir_path)]):
+        logging.info(f"create directory: {dir_path}")
+
+    Path(dir_path).mkdir(parents=True, exist_ok=True)
+    with open(configs_file_path, 'w') as fp:
+        yaml.dump(configs, fp)
+        logging.info(f"write to: {configs_file_path}, with: {configs}")
+
+    if debug:
+        configs = load_yaml_configs(configs_file_path)
+
+    return configs
 
 
 if "__main__" == __name__:
