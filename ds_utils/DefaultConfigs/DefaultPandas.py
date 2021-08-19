d40f9b072d20c086518317591a0bcea797b5040b
cttsai <cttsai@outlook.com>
2021-08-18 20:20:10 -0400

    fix display params

--- ds_utils/DefaultConfigs/DefaultPandas.py
+++ ds_utils/DefaultConfigs/DefaultPandas.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def configure_pandas_display():
-    pd.set_option('display.max_rows', 1000)
+    pd.set_option('display.max_rows', 10000)
     pd.set_option('display.max_columns', 1000)
     pd.set_option('display.width', 1000)
     return
