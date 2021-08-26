992b5b22eac96537bd3ebab249515b25001bc425
cttsai <cttsai@outlook.com>
2021-08-25 21:39:05 -0400

    adding filtering in ensemble creator

--- script/ensemble_configs_creator.py
+++ script/ensemble_configs_creator.py
@@ -3,7 +3,7 @@ import os
 import argparse
 import logging
 from glob import glob
-from typing import List
+from typing import List, Dict
 from itertools import combinations_with_replacement
 import pdb
 import pandas as pd
@@ -24,6 +24,7 @@ def parse_commandline() -> argparse.Namespace:
 
     default_output_dir: str = "../ensemble_configs/"
     default_run_configs_filepath: str = "../infer_configs/batch_ensemble_infer.yaml"
+    default_ensemble_script_filepath: str = "./ensemble.py"
 
     default_configs_pattern: str = "../configs/configs_baseline_*.yaml"
     default_model_name_stem: str = "ensemble_sel"
@@ -38,12 +39,16 @@ def parse_commandline() -> argparse.Namespace:
     parser.add_argument(
         "--ensemble-template", type=str, default=ensemble_template_file_path, help="ensemble template file path")
     parser.add_argument(
+        "--ensemble-script", type=str, default=default_ensemble_script_filepath, help="ensemble script")
+    parser.add_argument(
         "--run-template", type=str, default=run_template_file_path, help="run template file path")
     parser.add_argument(
         "--configs-pattern", type=str, default=default_configs_pattern, help="default configs patten")
     parser.add_argument("--output-dir", type=str, default=default_output_dir, help="directory to write out")
-    parser.add_argument("--low", type=int,default=2, help="min number of files to ensemble")
+    parser.add_argument("--low", type=int, default=2, help="min number of files to ensemble")
     parser.add_argument("--high", type=int, default=6, help="high number of files to ensemble")
+    parser.add_argument("--max-model-weight", type=float, default=.51, help="upper bound of weights in model ensemble")
+    parser.add_argument("--dry-run", action="store_true", help="dry run")
 
     args = parser.parse_args()
     return args
@@ -65,13 +70,14 @@ if "__main__" == __name__:
     logging.info(f"find {len(existing_config_files)} configs: {existing_config_files}")
 
     logging.info(f"start loading configs:")
-    existing_configs = list(map(
+    existing_configs: List[str] = list(map(
         lambda x: SolutionConfigs(root_resource_path=root_resource_path, configs_file_path=x), existing_config_files))
 
-    output_dirs = list(map(lambda x: str(Path(x.output_dir_).absolute()), existing_configs))
+    output_dirs: str = list(map(lambda x: str(Path(x.output_dir_).absolute()), existing_configs))
 
-    active_output_dirs = list(filter(lambda x: os.path.exists(x) and os.path.isdir(x), output_dirs))
-    config_file_output_dict = {k: v for k, v in zip(output_dirs, existing_config_files) if k in active_output_dirs}
+    active_output_dirs: List[str] = list(filter(lambda x: os.path.exists(x) and os.path.isdir(x), output_dirs))
+    config_file_output_dict: Dict[str, str] = {
+        k: v for k, v in zip(output_dirs, existing_config_files) if k in active_output_dirs}
 
     existing_config_files = list(config_file_output_dict.values())
     logging.info(f"available configs: {len(existing_config_files)}: {existing_config_files}")
@@ -84,9 +90,12 @@ if "__main__" == __name__:
     configs_all_ranges: List[str] = list()
     combinations_all_ranges = list(map(list, combinations_all_ranges))
     logging.info(f"available configs: {len(combinations_all_ranges)}")
+    combinations_all_ranges = list(filter(
+        lambda x: pd.Series(x).value_counts(normalize=True).max() < _args.max_model_weight, combinations_all_ranges))
+    logging.info(f"available configs after filtering {_args.max_model_weight}: {len(combinations_all_ranges)}")
+
     for i, pair in enumerate(combinations_all_ranges):
         _configs_template_copy = _configs_template.copy()
-
         model_name = "_".join([_args.model_name_stem, str(len(pair)), _configs_template_copy["ensemble_method"]])
         _configs_template_copy["model_name"] = model_name
         _configs_template_copy["ensemble_model_configs"] = pair
@@ -96,22 +105,29 @@ if "__main__" == __name__:
 
         hash_str = ds_utils.Utils.get_file_hash("./tmp.yaml")
         filepath: str = os.path.join(_args.output_dir, f"ensemble_{i:04d}_{hash_str}.yaml")
+        if _args.dry_run:
+            continue
+
         ds_utils.Utils.save_yaml_configs(_configs_template_copy, filepath)
         configs_all_ranges.append(filepath)
 
     # create run configs
     run_template = ds_utils.Utils.load_yaml_configs(_args.run_template)
+    logging.info(f"load template run script: {run_template}")
     run_compute_collection = list()
     for config_filepath in configs_all_ranges:
         _run_compute_template = {
             "command": ["--compute-eval", ],
             "config_file": config_filepath,
             "refresh_level": "predictions",
-            "script_file": "./ensemble.py",
+            "script_file": _args.ensemble_script,
             "script_type": "python"
         }
         # _run_compute_template["config_file"] = config_filepath
         run_compute_collection.append(_run_compute_template)
 
     run_template["compute"] = run_compute_collection
-    ds_utils.Utils.save_yaml_configs(run_template, _args.run_configs_filepath)
+    if not _args.dry_run:
+        ds_utils.Utils.save_yaml_configs(run_template, _args.run_configs_filepath)
+    else:
+        logging.info(f"dry run to write to: {_args.run_configs_filepath}:\n{run_template}")
