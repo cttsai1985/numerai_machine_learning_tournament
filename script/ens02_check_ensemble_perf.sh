322555075e084162f1e76e7e74701515c5748bfb
cttsai <cttsai@outlook.com>
2021-08-27 18:02:53 -0400

    update script

--- script/ens02_check_ensemble_perf.sh
+++ script/ens02_check_ensemble_perf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-# python3 output_valiation_overview.py --output-pattern "ensemble_sel_*" --corr-cut-off 0.96 --num-rows 2000
-# python3 output_valiation_overview.py --output-pattern "old_ens/ensemble_sel_*" --corr-cut-off 0.96 --num-rows 2000
+# python3 offline_model_overview.py --use-filter --output-pattern "ensemble_sel_*" --corr-cut-off 0.96 --num-rows 2000
+# python3 offline_model_overview.py --use-filter --output-pattern "old_ens/ensemble_sel_*" --corr-cut-off 0.96 --num-rows 2000
--- script/ens03_cleanup_output.sh
+++ script/ens03_cleanup_output.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-python3 housekeep.py --dry-run --destination-output ../input/numerai_tournament_resource/old_ens/ --destination-configs ../configs/not_active_ens/ --configs-pattern "../ensemble_configs/*.yaml" --output-pattern "ens*"
+python3 housekeep.py --dry-run --destination-output ../input/numerai_tournament_resource/old_ens/ --destination-configs ../configs/not_active_ens/ --configs-pattern "../ensemble_configs/*.yaml" --output-pattern "ensemble_sel_*"
 
 # python3 housekeep.py --dry-run --destination-output ../input/numerai_tournament_resource/old_ens/ --destination-configs ../configs/not_active_ens/ --configs-pattern "../ensemble_configs/*.yaml" --output-pattern "ens*"
