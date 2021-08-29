da9d5f4e5521ba2aa7b89a36eb7a338bcabf569a
cttsai <cttsai@outlook.com>
2021-08-29 12:00:24 -0400

    multi process support for run.py

--- script/run.py
+++ script/run.py
@@ -7,7 +7,8 @@ import argparse
 import logging
 import subprocess
 from pathlib import Path
-from typing import Dict, Any, List, Optional
+from multiprocessing import Pool, cpu_count
+from typing import Dict, Any, List, Optional, Tuple
 
 EXTERNAL_UTILS_LIB = os.path.join(Path().resolve().parent)
 sys.path.insert(0, EXTERNAL_UTILS_LIB)
@@ -80,6 +81,49 @@ def compile_base_command(
     return exec_command
 
 
+def compute_inference(params: Tuple):
+    execute_params, config_file, i, num_task_sequence = params
+    logging.info(f"inference: {i:8d} / {num_task_sequence}")
+    execute_config_file = execute_params["config_file"]
+    logging.info(f"inference with: {execute_config_file}")
+    execute_on_process(compile_infer(**execute_params))
+    return
+
+
+def compute_offline_evaluation(params: Tuple):
+    execute_params, config_file, i, num_task_sequence = params
+    logging.info(f"offline evaluation: {i:8d} / {num_task_sequence}")
+
+    execute_config_file = execute_params["config_file"]
+    offline_diagnostic_configs = config_file["offline_diagnostics"]
+    if not offline_diagnostic_configs:
+        return
+
+    logging.info(f"offline evaluate with: {execute_config_file}")
+    for diagnostic_params in offline_diagnostic_configs:
+        execute_on_process(compile_offline_diagnostics(**diagnostic_params, config_file=execute_config_file))
+
+    return
+
+
+def compute_online_evaluation(params: Tuple):
+    execute_params, config_file, i, num_task_sequence = params
+    logging.info(f"online evaluation: {i:8d} / {num_task_sequence}")
+
+    execute_config_file = execute_params["config_file"]
+    logging.info(f"online evaluate with: {execute_config_file}")
+    online_diagnostic_configs = config_file["online_diagnostics"]
+    if not online_diagnostic_configs:
+        return
+
+    for diagnostic_params in online_diagnostic_configs:
+        diagnostic_params.update(config_file["numerapi_configs"])
+        execute_on_process(
+            compile_online_diagnostics(config_file=execute_config_file, **diagnostic_params))
+
+    return
+
+
 def main(args: argparse.Namespace):
     if not (os.path.exists(_args.configs) and os.path.isfile(_args.configs)):
         logging.error(f"configuration is not usable. abort: {_args.configs}")
@@ -97,30 +141,31 @@ def main(args: argparse.Namespace):
     task_sequence = config_file.get("compute")
     if not task_sequence:
         task_sequence = list()
-    for i, execute_params in enumerate(task_sequence):
-        logging.info(f"progress: {i:8d} / {len(task_sequence)}")
-        execute_config_file = execute_params["config_file"]
-        logging.info(f"inference with: {execute_config_file}")
-        execute_on_process(compile_infer(**execute_params))
 
-        offline_diagnostic_configs = config_file["offline_diagnostics"]
-        if offline_diagnostic_configs:
-            logging.info(f"offline evaluate with: {execute_config_file}")
-            for diagnostic_params in offline_diagnostic_configs:
-                execute_on_process(compile_offline_diagnostics(**diagnostic_params, config_file=execute_config_file))
-        else:
-            logging.info(f"skip offline evaluation")
+    _num_task_sequence = len(task_sequence)
+    task_sequence = [
+        (_execute_params, config_file, i, _num_task_sequence) for i, _execute_params in enumerate(task_sequence)]
 
-        logging.info(f"online evaluate with: {execute_config_file}")
-        online_diagnostic_configs = config_file["online_diagnostics"]
-        if online_diagnostic_configs:
-            for diagnostic_params in online_diagnostic_configs:
-                diagnostic_params.update(config_file["numerapi_configs"])
-                execute_on_process(
-                    compile_online_diagnostics(config_file=execute_config_file, **diagnostic_params))
+    if args.n_jobs == 1:
+        list(map(lambda x: compute_inference(x), task_sequence))
+        list(map(lambda x: compute_offline_evaluation(x), task_sequence))
+    else:
+        n_jobs = args.n_jobs if args.n_jobs > 1 else cpu_count()
+        logging.info(f"multi processing: {n_jobs}")
+        with Pool(n_jobs) as p:
+            p.map(compute_inference, task_sequence)
 
-        else:
-            logging.info(f"skip online evaluation")
+            offline_diagnostic_configs = config_file["offline_diagnostics"]
+            if offline_diagnostic_configs:
+                p.map(compute_offline_evaluation, task_sequence)
+            else:
+                logging.info(f"skip offline evaluation")
+
+    online_diagnostic_configs = config_file["online_diagnostics"]
+    if online_diagnostic_configs:
+        list(map(compute_online_evaluation, task_sequence))
+    else:
+        logging.info(f"skip online evaluation")
 
     task_sequence = config_file.get("closure")
     if not task_sequence:
@@ -136,6 +181,7 @@ def parse_commandline() -> argparse.Namespace:
         description="execute a series of scripts", add_help=True,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--configs", type=str, default="infer_configs.yaml", help="configs file")
+    parser.add_argument("--n-jobs", type=int, default=1, help="how many process to use")
     args = parser.parse_args()
     return args
 
