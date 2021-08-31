8c6e3a40e6c41f9812473f3c57347da7fad7908b
cttsai <cttsai@outlook.com>
2021-08-31 19:08:01 -0400

    tweak bash

--- script/ens02_check_ensemble_perf.sh
+++ script/ens02_check_ensemble_perf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-# python3 offline_model_overview.py --use-filter --output-pattern "ensemble_sel_*" --corr-cut-off 0.96 --num-rows 2000
+# python3 offline_model_overview.py --use-filter --output-pattern "ensemble_sel_*" --corr-cut-off 0.96 --max-corr-std 0.295 --num-rows 2000
 # python3 offline_model_overview.py --use-filter --output-pattern "old_ens/ensemble_sel_*" --corr-cut-off 0.96 --num-rows 2000
