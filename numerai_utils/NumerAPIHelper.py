acd97f76eb206f183357b32650dbff5687946774
cttsai <cttsai@outlook.com>
2021-09-16 22:05:16 -0400

    update api for submit validation prediction

--- numerai_utils/NumerAPIHelper.py
+++ numerai_utils/NumerAPIHelper.py
@@ -1,26 +1,35 @@
+import json
 import os
 import logging
 import re
 import time
 from glob import glob
-from typing import Optional, List
+from typing import Optional, List, Iterable, Dict, Any
 from pathlib import Path
 import pandas as pd
 from numerapi import NumerAPI
 
-_MetricsToQuery = [
-    "validationSharpe",
-    "validationCorrelation",
-    "validationFeatureNeutralMean",
+_MetricsToQuery: List[str] = [
+    # Corr
+    "validationCorrSharpe",
+    "validationCorrMean",
+    "validationFeatureNeutralCorrMean",
 
-    "validationStd",
-    "validationMaxFeatureExposure",
+    # Risk
+    "validationCorrStd",
+    "validationFeatureCorrMax",
     "validationMaxDrawdown",
 
+    # MMC
     "validationCorrPlusMmcSharpe",
+    "validationCorrPlusMmcMean",
+    "validationCorrPlusMmcStd",
+    "validationMmcSharpe",
     "validationMmcMean",
+    "validationMmcStd",
+
     "validationCorrPlusMmcSharpeDiff",
-    "corrWithExamplePreds",
+    "examplePredsCorrMean"
 ]
 
 _FILENAMES: List[str] = [
@@ -38,26 +47,29 @@ _FILENAMES: List[str] = [
 class NumerAPIHelper:
     def __init__(
             self, root_dir_path: Optional[str] = None, api: Optional[NumerAPI] = None):
-        # TODO: wrap this into a json to init
-        self.api: NumerAPI = NumerAPI() if api is None else api
 
-        self.root_dir_path: str = os.path.join("..", "input", "numerai_tournament_resource")
-        if root_dir_path:
-            self.root_dir_path = root_dir_path
+        self.api: NumerAPI = api
+        if api is None:
+            self.api: NumerAPI = NumerAPI(
+                secret_key=os.environ.get("numerapiSecret", None), public_id=os.environ.get("numerapiPublicID", None))
 
+        self.root_dir_path = root_dir_path
+        if root_dir_path is None:
+            self.root_dir_path: str = os.path.join("..", "input", "numerai_tournament_resource")
         logging.info(f"current root path: {self.root_dir_path}")
 
         self.data_dir_path: str = os.path.join(self.root_dir_path, "latest_tournament_datasets")
         logging.info(f"data dir: {self.data_dir_path}")
 
-        self.filenames: List[str] = _FILENAMES
-
         self.current_round_str = f"{self.api.get_current_round():04d}"
-        self.round_identifier_template: str = f"numerai_tournament_round"
+        self.round_identifier_template: str = "numerai_tournament_round"
         self.current_round_identifier: str = "_".join([self.round_identifier_template, self.current_round_str])
         logging.info(f"current round identifier: {self.current_round_identifier}")
         self.latest_round_identifier: Optional[str] = None
         self.find_latest_round_identifier()
+
+        #
+        self.filenames: List[str] = _FILENAMES
 
     def find_latest_round_identifier(self, dir_path: Optional[str] = None) -> Optional[str]:
         if self.latest_round_identifier:
@@ -104,59 +116,60 @@ class NumerAPIHelper:
 
         if self.is_latest_round_identifier_current_ and not refresh:
             logging.info(f"skip download since datasets are up-to-date")
-            return
+            return self
 
         if not filenames:
             filenames = self.filenames
 
         Path(self.data_dir_path).mkdir(parents=True, exist_ok=True)
-        for filename in filenames:
+        file_paths: Iterable[str] = list(map(lambda _f: os.path.join(self.data_dir_path, _f), filenames))
 
-            _filepath: str = os.path.join(self.data_dir_path, filename)
-            if os.path.exists(_filepath) and refresh:
-                os.remove(_filepath)
-            self.api.download_dataset(filename=filename, dest_path=_filepath)
-            logging.info(f"\n{filename} download finished.")
+        if refresh:
+            for _filepath in file_paths:
+                if os.path.exists(_filepath):
+                    os.remove(_filepath)
+                logging.info(f"remove {_filepath}")
+
+        for _filename, _filepath in zip(filenames, file_paths):
+            self.api.download_dataset(filename=_filename, dest_path=_filepath)
+            logging.info(f"\n{_filename} download finished.")
 
         logging.info(f"\nall requested datasets updated: " + ", ".join(filenames))
         self.update_round_identifier_to_current()
         return self
 
-    def retrieve_submission_diagnostics(self, model_id: str, max_retry: int = 10, sleep_time: int = 30) -> pd.Series:
-        time.sleep(15)
-        series = pd.Series(self.api.submission_status(model_id))
-        for i in range(max_retry):
-            # noinspection PyBroadException
-            try:
-                while True:
-                    time.sleep(sleep_time)
-                    logging.info(f"retry {i}, waiting for valid submission_status return.")
-                    series = pd.Series(self.api.submission_status(model_id))
-                    if not series.isna().all():
-                        return series
-
-            except Exception:
-                logging.error(f"retry {i} times", exc_info=True)
-
-        return series
-
-    def evaluate_online_diagnostics(
-            self, model_name: Optional[str] = None, dir_path: Optional[str] = None, refresh: bool = False):
+    def _compose_submit_filepath(self, filename: str, dir_path: Optional[str] = None):
         if not dir_path:
             dir_path = self.root_dir_path
 
-        result_filepath = os.path.join(self.result_dir_current_round_, Path(dir_path).name + ".csv")
-        if os.path.exists(result_filepath) and not refresh:
-            logging.info(f"skip upload since result {result_filepath} exists and not refresh")
-            return True
+        filepath = os.path.join(dir_path, filename)
+        return filepath
+
+    def submit_diagnostics(
+            self, model_name: Optional[str] = None, dir_path: Optional[str] = None,
+            filename: str = "validation_predictions.csv"):
+        filepath = self._compose_submit_filepath(filename=filename, dir_path=dir_path)
 
         # submit submission
         model_id = self.api.get_models()[model_name]  # get model_id
-        logging.info(f"model w/ its model_id: {model_name} ({model_id})")
-        self.api.upload_predictions(
-            os.path.join(dir_path, "validation_predictions.csv"), model_id=model_id, version=2)
-        # retrieve submission
-        series = self.retrieve_submission_diagnostics(model_id)
+        logging.info(f"model ({model_name}) w/ its model_id: ({model_id})")
+
+        diagnostics_id = self.api.upload_diagnostics(file_path=filepath, model_id=model_id)
+        logging.info(f"diagnostics of {model_name} ({model_id}): {diagnostics_id}")
+
+        results = self.api.diagnostics(model_id=model_id, diagnostics_id=diagnostics_id)
+        self.save_diagnostics(results=results, dir_path=dir_path)
+        return self
+
+    def get_diagnostics(
+            self, model_name: Optional[str] = None, diagnostics_id: Optional[str] = None) -> List[Dict[str, Any]]:
+        model_id = self.api.get_models()[model_name]  # get model_id
+        logging.info(f"model ({model_name}) w/ its model_id: ({model_id})")
+        return self.api.diagnostics(model_id=model_id, diagnostics_id=diagnostics_id)
+
+    @staticmethod
+    def _extract_all_era_info_from_json(results: List[Dict[str, Any]]) -> pd.DataFrame:
+        series: Dict[str, Any] = pd.Series(results[0])
 
         # process
         metrics = series[_MetricsToQuery]
@@ -166,23 +179,34 @@ class NumerAPIHelper:
 
         df = pd.concat([metrics.rename("score"), metrics_rating.rename("rating")], axis=1)
         df.index.name = "attr"
-        logging.info(f"model diagnostics: {model_name} ({model_id})\n{df}")
-        df.to_csv(os.path.join(dir_path, "online_model_diagnostics.csv"), )
+        return df
 
-        Path(self.result_dir_current_round_).mkdir(parents=True, exist_ok=True)
+    def save_diagnostics(self, results: List[Dict[str, Any]], dir_path: Optional[str] = None,):
+        result_filepath = os.path.join(self.result_dir_current_round_, Path(dir_path).name + ".json")
+        with open(result_filepath, "w") as f:
+            json.dump(results, fp=f)
+
+        result_filepath = os.path.join(self.result_dir_current_round_, Path(dir_path).name + ".csv")
+        df = self._extract_all_era_info_from_json(results=results)
         df.to_csv(result_filepath)
-        logging.info(f"write online diagnostics to {result_filepath}")
         return self
 
-    def submit_prediction(
-            self, model_name: Optional[str] = None, dir_path: Optional[str] = None, refresh: bool = False):
-        if not dir_path:
-            dir_path = self.root_dir_path
-
-        filepath = os.path.join(dir_path, "tournament_predictions.csv")
+    def submit_predictions(
+            self, model_name: Optional[str] = None, dir_path: Optional[str] = None,
+            filename: str = "tournament_predictions.csv"):
+        filepath = self._compose_submit_filepath(filename=filename, dir_path=dir_path)
 
         # submit submission
         model_id = self.api.get_models()[model_name]  # get model_id
-        logging.info(f"model w/ its model_id: {model_name} ({model_id})")
+        logging.info(f"model ({model_name}) w/ its model_id: ({model_id})")
+
         self.api.upload_predictions(filepath, model_id=model_id, version=2)
+        logging.info(f"predictions uploaded w/ its model_id: {model_name} ({model_id})")
         return self
+
+    def do_submission(
+            self, model_name: Optional[str] = None, dir_path: Optional[str] = None,
+            diagnostics_filename: str = "validation_predictions.csv",
+            predictions_filename: str = "tournament_predictions.csv"):
+        self.submit_diagnostics(model_name=model_name, dir_path=dir_path, filename=diagnostics_filename)
+        self.submit_predictions(model_name=model_name, dir_path=dir_path, filename=predictions_filename)
--- script/upload_submission.py
+++ script/upload_submission.py
@@ -1,10 +1,8 @@
 import sys
 import os
-import json
 import argparse
 import logging
 from pathlib import Path
-from numerapi import NumerAPI
 
 EXTERNAL_UTILS_LIB = os.path.join(Path().resolve().parent)
 sys.path.insert(0, EXTERNAL_UTILS_LIB)
@@ -19,10 +17,7 @@ def parse_commandline() -> argparse.Namespace:
         description="Model Diagnostics", add_help=True,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--configs", type=str, default="./configs.yaml", help="configs file")
-    parser.add_argument("--numerapiPublicID", type=str, default="", help="numerapi public ID")
-    parser.add_argument("--numerapiSecret", type=str, default="", help="numerapi secret")
     parser.add_argument("--model-name", type=str, default="cttsai", help="")
-    parser.add_argument("--refresh", action="store_true", help="force upload predictions")
     args = parser.parse_args()
     return args
 
@@ -33,11 +28,8 @@ if "__main__" == __name__:
 
     root_resource_path: str = "../input/numerai_tournament_resource/"
     _args = parse_commandline()
-    logging.info(f"NumerAI credentials: public_id={_args.numerapiPublicID}, secret={_args.numerapiSecret}")
 
     configs = SolutionConfigs(root_resource_path=root_resource_path, configs_file_path=_args.configs)
 
-    napi = NumerAPI(secret_key=_args.numerapiSecret, public_id=_args.numerapiPublicID)
-    helper = NumerAPIHelper(root_dir_path=root_resource_path, api=napi)
-    helper.submit_prediction(model_name=_args.model_name, dir_path=configs.output_dir_, refresh=_args.refresh)
-    # helper.evaluate_online_diagnostics(model_name=_args.model_name, dir_path=configs.output_dir_, refresh=_args.refresh)
+    helper = NumerAPIHelper(root_dir_path=root_resource_path)
+    helper.do_submission(model_name=_args.model_name, dir_path=configs.output_dir_, )
