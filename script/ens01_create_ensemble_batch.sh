c4a8e1151c7fa12b85619d1ddaa71260b43aeb47
cttsai <cttsai@outlook.com>
2021-09-21 09:48:41 -0400

    batch update

--- script/ens01_create_ensemble_batch.sh
+++ script/ens01_create_ensemble_batch.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-python3 ensemble_configs_creator.py --dry-run --low 3 --high 7 --configs-pattern "../configs/configs_baseline_*.yaml" --output-dir ../ensemble_configs/ --run-configs-filepath ../infer_configs/batch_ensemble_infer.yaml --max-model-weight 0.5
+python3 ensemble_configs_creator.py --dry-run --low 7 --high 11 --configs-pattern "../configs/configs_baseline_*.yaml" --output-dir ../ensemble_configs/ --run-configs-filepath ../infer_configs/batch_ensemble_infer.yaml --max-model-weight 0.3
+python3 ensemble_configs_creator.py --dry-run --low 5 --high 9 --configs-pattern "../configs/configs_baseline_*.yaml" --output-dir ../ensemble_configs/ --run-configs-filepath ../infer_configs/batch_ensemble_infer.yamlensemble_infer.yaml --max-model-weight 0.3
