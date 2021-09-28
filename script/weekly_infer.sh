37d88a9067ba8a8fcccb18dbd97d3e416293b9ac
cttsai <cttsai@outlook.com>
2021-09-28 10:00:57 -0400

    more weekly rountine

--- script/weekly_infer.sh
+++ script/weekly_infer.sh
@@ -6,3 +6,9 @@ python3 run.py --configs ../infer_configs/infer_focus_risk.yaml
 python3 run.py --configs ../infer_configs/infer_focus_sharpe.yaml
 python3 run.py --configs ../infer_configs/infer_focus_best.yaml
 
+
+python3 run.py --configs ../infer_configs/infer_reference.yaml
+python3 run.py --configs ../infer_configs/infer_reference_arthur.yaml
+python3 run.py --configs ../infer_configs/infer_reference_william.yaml
+python3 run.py --configs ../infer_configs/infer_reference_example.yaml
+
