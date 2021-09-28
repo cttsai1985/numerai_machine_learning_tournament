d5bd852a996e8328376b0e5f5fd69b22ed7dc26e
cttsai <cttsai@outlook.com>
2021-09-28 17:44:42 -0400

    run shell script

---
+++ 01_init_env.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+DOCKER_IMAGE=cttsai1985/ml-env-rapids:latest
+
+SHM_SIZE=4G
+
+RootSrcPath=${PWD}
+DockerRootSrcPath=/root/src/
+
+DataPath=${PWD}/input
+DockerDataPath=/root/src/input
+
+RootPort1=8088
+DockerRootPort1=8888
+
+RootPort2=6066
+DockerRootPort2=6666
+
+GPU_DEVICE='"device=0"'
+
+WORKDIR="/root/src/script"
+
+# docker rm $(docker ps -a -q)
+
+CMD="bash ./init_env.sh"
+
+echo docker run -i -t --gpus ${GPU_DEVICE} -p $RootPort1:$DockerRootPort1 -p $RootPort2:$DockerRootPort2 -e PYTHONPATH=/root/src -v ${RootSrcPath}:${DockerRootSrcPath} -v $(readlink -f ${DataPath}):${DockerDataPath} --shm-size $SHM_SIZE --workdir=${WORKDIR} --env-file env.local $DOCKER_IMAGE $CMD
+
+docker run -i -t --gpus ${GPU_DEVICE} -p $RootPort1:$DockerRootPort1 -p $RootPort2:$DockerRootPort2 -e PYTHONPATH=/root/src -v ${RootSrcPath}:${DockerRootSrcPath} -v $(readlink -f ${DataPath}):${DockerDataPath} --shm-size $SHM_SIZE --workdir=${WORKDIR} --env-file env.local $DOCKER_IMAGE $CMD
--- script/weekly_infer.sh
+++ script/weekly_infer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+python3 download_new_datasets.py --refresh --update
+
 python3 run.py --configs ../infer_configs/infer_baseline_cttsai.yaml
 python3 run.py --configs ../infer_configs/infer_focus_fnc.yaml
 python3 run.py --configs ../infer_configs/infer_focus_risk.yaml
@@ -9,6 +11,8 @@ python3 run.py --configs ../infer_configs/infer_focus_best.yaml
 
 python3 run.py --configs ../infer_configs/infer_reference.yaml
 python3 run.py --configs ../infer_configs/infer_reference_arthur.yaml
+python3 run.py --configs ../infer_configs/infer_reference_ben.yaml
+python3 run.py --configs ../infer_configs/infer_reference_thomas.yaml
 python3 run.py --configs ../infer_configs/infer_reference_william.yaml
 python3 run.py --configs ../infer_configs/infer_reference_example.yaml
 
