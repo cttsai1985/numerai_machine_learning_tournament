58b9e8137758d2ff7f97f64b7f8264b630127ce1
cttsai <cttsai@outlook.com>
2021-10-13 14:45:28 -0400

    update weekly infer

--- script/weekly_infer.sh
+++ script/weekly_infer.sh
@@ -2,13 +2,20 @@
 
 python3 download_new_datasets.py --refresh --update
 
+#
 python3 run.py --configs ../infer_configs/infer_baseline_cttsai.yaml
+
+# focus
 python3 run.py --configs ../infer_configs/infer_focus_fnc.yaml
 python3 run.py --configs ../infer_configs/infer_focus_risk.yaml
 python3 run.py --configs ../infer_configs/infer_focus_sharpe.yaml
 python3 run.py --configs ../infer_configs/infer_focus_best.yaml
 
+# mix
+python3 run.py --configs ../infer_configs/infer_mix_cat_ranker.yaml
+python3 run.py --configs ../infer_configs/infer_mix_light_clf.yaml
 
+# reference
 python3 run.py --configs ../infer_configs/infer_reference.yaml
 python3 run.py --configs ../infer_configs/infer_reference_arthur.yaml
 python3 run.py --configs ../infer_configs/infer_reference_ben.yaml
