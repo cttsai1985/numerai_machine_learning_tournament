96792623ed37750cdea0069af48ff7eb694a2628
cttsai <cttsai@outlook.com>
2021-10-18 16:09:49 -0400

    util script like ranking and house keep

---
+++ 04_housekeep.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+DOCKER_IMAGE=cttsai1985/ml-env-rapids:latest
+
+SHM_SIZE=4G
+
+RootSrcPath=${PWD}
+DockerRootSrcPath=/root/src/
+
+DataPath=${PWD}/input
+DockerDataPath=/root/src/input
+
+RootPort1=8088
+DockerRootPort1=8888
+
+RootPort2=6066
+DockerRootPort2=6666
+
+CPU_COUNTS="16"
+GPU_DEVICE='"device=0"'
+
+WORKDIR="/root/src/script"
+
+docker rm $(docker ps -a -q)
+
+CMD="bash housekeep_adhoc_solutions.sh"
+
+echo docker run -i -t --cpus=${CPU_COUNTS} --gpus ${GPU_DEVICE} -p $RootPort1:$DockerRootPort1 -p $RootPort2:$DockerRootPort2 -e PYTHONPATH=/root/src -v ${RootSrcPath}:${DockerRootSrcPath} -v $(readlink -f ${DataPath}):${DockerDataPath} --shm-size $SHM_SIZE --workdir=${WORKDIR} --env-file env.local $DOCKER_IMAGE $CMD
+
+docker run -i -t --cpus=${CPU_COUNTS} --gpus ${GPU_DEVICE} -p $RootPort1:$DockerRootPort1 -p $RootPort2:$DockerRootPort2 -e PYTHONPATH=/root/src -v ${RootSrcPath}:${DockerRootSrcPath} -v $(readlink -f ${DataPath}):${DockerDataPath} --shm-size $SHM_SIZE --workdir=${WORKDIR} --env-file env.local $DOCKER_IMAGE $CMD
+
---
+++ script/housekeep_adhoc_solutions.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+python3 housekeep.py --destination-output ../input/numerai_tournament_resource/old_adhoc/ --destination-configs ../configs/not_active_adhoc/ --configs-pattern "../ensemble_configs/*.yaml" --output-pattern "ensemble_*" --output-file-lookup validation_predictions.csv
+
+python3 housekeep.py --destination-output ../input/numerai_tournament_resource/old_adhoc/ --destination-configs ../configs/not_active_adhoc/ --configs-pattern "../neutralize_configs/*.yaml" --output-pattern "neutralize_*" --output-file-lookup validation_predictions.csv
