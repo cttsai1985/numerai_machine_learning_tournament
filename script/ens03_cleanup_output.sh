8062d1df9f23e85fef4cf0824a0d0306614a7b1c
cttsai <cttsai@outlook.com>
2021-10-20 13:15:42 -0400

    some more cmd for house keep

--- script/ens03_cleanup_output.sh
+++ script/ens03_cleanup_output.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+
+python3 housekeep.py --dry-run --destination-output ../input/numerai_tournament_resource/old/ --destination-configs ../configs/not_active_catb/ --configs-pattern "../configs/*pairlogitp*.yaml" --output-pattern "*pairlogitp*" --output-file-lookup validation_predictions.csv
+
 python3 housekeep.py --dry-run --destination-output ../input/numerai_tournament_resource/old_ens/ --destination-configs ../configs/not_active_ens/ --configs-pattern "../ensemble_configs/*.yaml" --output-pattern "ensemble_sel_*" --output-file-lookup validation_predictions.csv
+
+python3 housekeep.py --dry-run --destination-output ../input/numerai_tournament_resource/old_neu/ --destination-configs ../configs/not_active_neu/ --configs-pattern "../neutralize_configs/*.yaml" --output-pattern "neutralize_*" --output-file-lookup validation_predictions.csv
 
 # python3 housekeep.py --dry-run --destination-output ../input/numerai_tournament_resource/old_ens/ --destination-configs ../configs/not_active_ens/ --configs-pattern "../ensemble_configs/*.yaml" --output-pattern "ens*" --output-file-lookup  validation_model_diagnostics.csv
