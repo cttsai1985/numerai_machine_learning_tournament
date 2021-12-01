3805ce0ed895962de9eaa228f2e763b5ae2d03be
cttsai <cttsai@outlook.com>
2021-11-30 22:04:53 -0500

    use more configs in feature target corr

--- script/init_env.sh
+++ script/init_env.sh
@@ -3,4 +3,6 @@
 python3 download_new_datasets.py --refresh 
 
 python3 analyze_data_file_columns.py
+
 python3 feature_target_corr.py --configs ../configs/configs_baseline_lgbm_gbdt.yaml
+python3 feature_target_corr.py --configs ../configs/configs_neutralized_lgbm_gbdt.yaml
