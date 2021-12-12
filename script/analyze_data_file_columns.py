b5729c3e1b9605a2868790411b9c88ce49651ade
cttsai <cttsai@outlook.com>
2021-12-12 08:11:31 -0500

    load numerai official feature subsets

--- script/analyze_data_file_columns.py
+++ script/analyze_data_file_columns.py
@@ -20,10 +20,11 @@ if "__main__" == __name__:
 
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
+    # parse all feature in data
     target_columns = sorted(list(filter(lambda x: x.startswith("target_"), columns)))
     output_filename: str = os.path.join(output_dir, ft.default_target_collection_filename)
     with open(output_filename, "w") as f:
-        json.dump(target_columns, f)
+        json.dump(target_columns, f, sort_keys=True, indent=4)
         print(f"save {len(target_columns)} targets to {output_filename}")
 
     feature_columns = sorted(list(filter(lambda x: x.startswith("feature_"), columns)))
@@ -31,3 +32,14 @@ if "__main__" == __name__:
     with open(output_filename, "w") as f:
         json.dump(feature_columns, f)
         print(f"save {len(feature_columns)} features to {output_filename}")
+
+    # numer ai selection
+    default_feature_stats_filename: str = os.path.join(ft.default_data_dir, "features.json")
+    with open(default_feature_stats_filename, 'r') as f:
+        obj = json.load(f)
+
+    for feature_set_name, feature_columns in obj['feature_sets'].items():
+        output_filename: str = os.path.join(output_dir, f"features_{feature_set_name}.json")
+        with open(output_filename, "w") as f:
+            json.dump(feature_columns, f, sort_keys=True, indent=4)
+            print(f"save {len(feature_columns)} features to {output_filename}")
